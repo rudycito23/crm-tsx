3cc3a24db883c82da033fb51aebb87f473b5f68c
rudycito23 <rrlucas@live.com>
2023-06-04 00:32:26 -0500

    Added folders; began Header component.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,18 @@
 import "../styles/Header.css";
+import { MdPermIdentity } from "react-icons/md"
 
 export const Header = () => {
     return (
       <>
-        <div className="w-screen bg-black">
-          <h1 className="text-slate-200">Header</h1>
+        <div className="w-screen bg-slate-300 flex">
+          <div>
+            <button>
+              <span>
+                <MdPermIdentity size={20} />
+                <span>Login</span>
+              </span>
+            </button>
+          </div>
         </div>
       </>
     );
