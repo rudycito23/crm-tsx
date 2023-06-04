5b20b0cd5c127b74c58b845a7e0c7d872fd6ecaf
rudycito23 <rrlucas@live.com>
2023-06-04 15:36:40 -0500

    Added Login and Try it now buttons; in progress.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,20 @@
 import "../styles/Header.css";
-import { MdPermIdentity } from "react-icons/md"
+import { MdPermIdentity } from "react-icons/md";
 
 export const Header = () => {
-    return (
-      <>
-        <div className="w-screen bg-slate-300 flex">
-          <div>
-            <button>
-              <span>
-                <MdPermIdentity size={20} />
-                <span>Login</span>
-              </span>
-            </button>
-          </div>
+  return (
+    <>
+      <div className="w-screen bg-slate-300 flex flex-row-reverse">
+        <div className="m-4 flex justify-evenly items-center gap-1">
+          <button className="flex items-center space-x-2 border-solid border-2 border-sky-500 p-2">
+            <MdPermIdentity size={20} />
+            <span>Login</span>
+          </button>
+          <button className="flex items-center space-x-2 border-solid border-2 border-sky-500 p-2">
+            Try it free
+          </button>
         </div>
-      </>
-    );
-}
+      </div>
+    </>
+  );
+};
